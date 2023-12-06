9b4507d339e6294b5a9cb9dd6196055ac9955f53
huegi <m.huegi@outlook.com>
2023-12-06 23:30:05 +0100

    updated logger

--- src/naneos/logger/custom_logger.py
+++ src/naneos/logger/custom_logger.py
@@ -1,27 +1,42 @@
 import logging
+from typing import Optional
 
 
 class CustomFormatter(logging.Formatter):
-    grey = "\x1b[38;20m"
-    green = "\x1b[32;20m"
-    yellow = "\x1b[33;20m"
-    red = "\x1b[31;20m"
-    bold_red = "\x1b[31;1m"
-    reset = "\x1b[0m"
-    format = (
-        "%(asctime)s - %(name)s - %(levelname)s - %(message)s (%(filename)s:%(lineno)d)"
+    _grey = "\x1b[38;20m"
+    _green = "\x1b[32;20m"
+    _yellow = "\x1b[33;20m"
+    _red = "\x1b[31;20m"
+    _bold_red = "\x1b[31;1m"
+    _reset = "\x1b[0m"
+    _format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s (%(filename)s:%(lineno)d)"
+    _FORMATS_TERMINAL: tuple[tuple[int, str], ...] = (
+        (logging.DEBUG, _grey + _format + _reset),
+        (logging.INFO, _green + _format + _reset),
+        (logging.WARNING, _yellow + _format + _reset),
+        (logging.ERROR, _red + _format + _reset),
+        (logging.CRITICAL, _bold_red + _format + _reset),
+    )
+    _FORMATS_SAVE: tuple[tuple[int, str], ...] = (
+        (logging.DEBUG, _format),
+        (logging.INFO, _format),
+        (logging.WARNING, _format),
+        (logging.ERROR, _format),
+        (logging.CRITICAL, _format),
     )
 
-    FORMATS = {
-        logging.DEBUG: grey + format + reset,
-        logging.INFO: green + format + reset,
-        logging.WARNING: yellow + format + reset,
-        logging.ERROR: red + format + reset,
-        logging.CRITICAL: bold_red + format + reset,
-    }
+    def __init__(self, terminal: bool = False, fmt: Optional[str] = None):
+        self._FORMATS = self._FORMATS_TERMINAL if terminal else self._FORMATS_SAVE
+        if fmt is None:
+            fmt = self._format
+        super().__init__(fmt=fmt)
 
     def format(self, record):
-        log_fmt = self.FORMATS.get(record.levelno)
+        log_fmt = next(
+            (x[1] for x in self._FORMATS if x[0] == record.levelno),
+            self._FORMATS[0][1],
+        )
+
         formatter = logging.Formatter(log_fmt)
         return formatter.format(record)
 
@@ -30,12 +45,12 @@ def get_naneos_logger(name: str, level: int = logging.INFO):
     logger = logging.getLogger(name)
     logger.setLevel(level)
 
-    formatter_file = logging.Formatter("%(asctime)s - %(filename)s - %(message)s")
+    formatter_file = CustomFormatter(terminal=False)
     file_handler = logging.FileHandler("naneos-devices.log")
     file_handler.setLevel(level)
     file_handler.setFormatter(formatter_file)
 
-    formatter_stream = CustomFormatter("%(asctime)s - %(filename)s - %(message)s")
+    formatter_stream = CustomFormatter(terminal=True)
     stream_handler = logging.StreamHandler()
     stream_handler.setLevel(level)
     stream_handler.setFormatter(formatter_stream)
@@ -44,3 +59,12 @@ def get_naneos_logger(name: str, level: int = logging.INFO):
     logger.addHandler(file_handler)
 
     return logger
+
+
+if __name__ == "__main__":
+    logger = get_naneos_logger(__name__, logging.DEBUG)
+    logger.debug("debug message")
+    logger.info("info message")
+    logger.warning("warning message")
+    logger.error("error message")
+    logger.critical("critical message")
