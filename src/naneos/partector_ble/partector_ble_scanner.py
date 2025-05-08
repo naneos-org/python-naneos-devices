527fb738948c38d8d4d1d10f5e672a94fc22ee26
huegi <m.huegi@outlook.com>
2025-05-08 09:26:56 +0200

    Refactor PartectorBleScanner to improve structure and enhance scanning functionality

--- src/naneos/partector_ble/partector_ble_scanner.py
+++ src/naneos/partector_ble/partector_ble_scanner.py
@@ -8,26 +8,63 @@ from naneos.partector_ble.partector_ble_decoder import PartectorBleDecoder
 
 
 class PartectorBleScanner:
-    async def _detection_callback(self, device: BLEDevice, data: AdvertisementData) -> None:
-        """Handles all the callbacks from the BleakScanner used in the scan method.
+    def __init__(self, loop: asyncio.AbstractEventLoop, auto_start: bool) -> None:
+        self._loop = loop
+        self._stop_event = asyncio.Event()
+        self._stop_event.set()  # stopped by default
+
+        if auto_start:
+            self.start()
+
+    def __del__(self) -> None:
+        """Destructor for the scanner."""
+        self.stop()
+
+    def start(self) -> None:
+        """Starts the scanner."""
+        if not self._stop_event.is_set():
+            print("Scanner already started")
+            return None
+
+        self._stop_event.clear()
+        self._loop.create_task(self.scan())
+
+    def stop(self) -> None:
+        """Stops the scanner."""
+        self._stop_event.set()
+
+    async def _detection_callback(self, device: BLEDevice, adv: AdvertisementData) -> None:
+        """Handles the callbacks from the BleakScanner used in the scan method.
 
         Args:
             device (BLEDevice): Bleak BLEDevice object
             data (AdvertisementData): Bleak AdvertisementData object
         """
-        if device.name not in ["P2", "PartectorBT"]:
+
+        if device.name not in {"P2", "PartectorBT"}:  # P2 on windows, PartectorBT on linux / mac
             return None
 
-        print(PartectorBleDecoder.decode_std_chareristic(data))
+        print(PartectorBleDecoder.decode_std_chareristic(adv))
 
     async def scan(self) -> None:
         """Scans for BLE devices and calls the _detection_callback method for each device found."""
-        async with BleakScanner(self._detection_callback) as scanner:
-            await scanner.start()
-            await asyncio.sleep(2.0)
-            await scanner.stop()
+
+        scanner = BleakScanner(self._detection_callback)
+
+        while not self._stop_event.is_set():
+            async with scanner:
+                await asyncio.sleep(1.0)
+
+
+async def main():
+    loop = asyncio.get_event_loop()
+    scanner = PartectorBleScanner(loop=loop, auto_start=True)
+    try:
+        await asyncio.sleep(5)  # Asynchronous sleep to allow the loop to run
+    finally:
+        scanner.stop()
+        await asyncio.sleep(1)  # Allow cleanup tasks to complete
 
 
 if __name__ == "__main__":
-    scanner = PartectorBleScanner()
-    asyncio.run(scanner.scan())
+    asyncio.run(main())
