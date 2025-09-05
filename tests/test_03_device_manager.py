fa0f74fd22f6cca8855a3c345a09da48fdde8595
Mario Huegi <mario.huegi@naneos.ch>
2025-09-05 18:07:25 +0200

    Dev finish naneos device manager v1.1.3 (#23)
    * Refactor upload timer display and enhance BLE connection logging
    
    - Change upload timer display to show whole seconds instead of one decimal place.
    - Add NaN check for new device data points to prevent errors.
    - Update logger level from INFO to WARNING for BLE connection to reduce log verbosity.
    - Implement waiting mechanism to manage connection attempts and timeouts more effectively.
    - Improve test for NaneosDeviceManager to include more robust handling of keyboard interrupts.
    
    * Enhance NaneosDeviceManager to support dynamic enabling/disabling of serial and BLE connections
    
    * Enhance NaneosDeviceManager to support configurable serial, BLE, and upload settings
    
    * Refactor NaneosDeviceManager to support configurable upload interval and improve logging
    
    * Fix gathering interval logging and ensure next upload time is updated correctly
    
    * Enhance NaneosDeviceManager with output queue support and improved runtime controls
    
    * Update import path for NaneosDeviceManager and bump version to 1.1.3

--- tests/test_03_device_manager.py
+++ tests/test_03_device_manager.py
@@ -1,13 +1,35 @@
+import os
+import signal
+import threading
 import time
+
+import pytest
 
 from naneos.manager import NaneosDeviceManager
 
 
+def raise_keyboard_interrupt():
+    os.kill(os.getpid(), signal.SIGINT)
+
+
+@pytest.mark.timeout(30)
 def test_naneos_device_manager():
+    timer = threading.Timer(20, raise_keyboard_interrupt)  # trigger keyboard interrupt after 20s
+    timer.start()
+
     manager = NaneosDeviceManager()
     manager.start()
 
-    time.sleep(40)  # Allow some time for the manager to start
+    try:
+        while True:
+            time.sleep(1)
+            print(f"Seconds until next upload: {manager.get_seconds_until_next_upload():.0f}")
+            print(manager.get_connected_serial_devices())
+            print(manager.get_connected_ble_devices())
+            print()
+    except KeyboardInterrupt:
+        manager.stop()
+        manager.join()
+        print("NaneosDeviceManager stopped.")
 
-    manager.stop()
-    manager.join()
+    timer.cancel()
